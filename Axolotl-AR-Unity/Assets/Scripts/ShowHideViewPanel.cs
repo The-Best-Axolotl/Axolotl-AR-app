1b51612dc15ee9bcfe5b96763ac841182e9c5643
Aisven Devanand <adeva042@uottawa.ca>
2020-11-05 20:59:27 -0500

    ViewPanel show and hide

---
+++ Axolotl-AR-Unity/Assets/Scripts/ShowHideViewPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideViewPanel : MonoBehaviour
+{
+
+    public GameObject panel;
+    int counter;
+
+    public void showhidePanel()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            panel.gameObject.SetActive(false);
+        }
+        else
+        {
+            panel.gameObject.SetActive(true);
+        }
+
+    }
+
+}
