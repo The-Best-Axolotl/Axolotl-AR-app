64dcf33275b3be37ae66d7db7b8ef310ad5ea66f
yasseralzalak <yalza085@uottawa.ca>
2020-11-23 19:01:42 -0500

    Gaze Interactions and some new 3D models
    I tried to delete as much useless 3D models so it wont take long to import

---
+++ Axolotl-AR-Unity/Assets/Gaze Interaction/Gaze.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Gaze : MonoBehaviour
+{
+    List<InfoBehavior> infos = new List<InfoBehavior>();
+
+    void Start()
+    {
+        infos = FindObjectsOfType<InfoBehavior>().ToList();  
+    }
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position,transform.forward, out RaycastHit hitt))
+        {
+            GameObject go = hitt.collider.gameObject;
+            if (go.CompareTag("hasInfo"))
+            {
+                print("HERE");
+                Openinfo(go.GetComponent<InfoBehavior>());
+            }
+        }
+        else
+        {
+            CloseAll();
+        }
+    }
+    void Openinfo(InfoBehavior desiredInfo)
+    {
+        foreach(InfoBehavior info in infos)
+        {
+            if(info == desiredInfo)
+            {
+                info.OpenInfo();
+            }
+            else
+            {
+                info.CLoseInfo();
+            }
+        }
+    }
+
+    void CloseAll()
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            info.CLoseInfo();
+        }
+    }
+}
