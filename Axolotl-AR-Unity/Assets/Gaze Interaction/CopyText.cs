effc6efa091a525d0d75d3d2236ac0773c0a8443
Aisven Devanand <adeva042@uottawa.ca>
2020-11-29 22:39:33 -0500

    CommentFunction

---
+++ Axolotl-AR-Unity/Assets/Gaze Interaction/CopyText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CopyText : MonoBehaviour
+{
+    public string comment;
+    public InputField write;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        comment = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<TextMeshPro>().text = comment;
+    }    
+
+    public void Clear()
+    {
+        write.text = "";
+    }
+
+}
