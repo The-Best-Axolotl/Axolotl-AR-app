264bf369c9cc8e2804cbaaeccfb5fd7d064cc9ca
Aisven Devanand <adeva042@uottawa.ca>
2020-12-02 10:46:31 -0500

    Added comment to share mode

---
+++ Axolotl-AR-Unity/Assets/CopyTextinGaze.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CopyTextinGaze : MonoBehaviour
+{
+    public string comment;
+    public InputField write;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        comment = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponentInChildren<TextMeshPro>().text = comment;
+    }
+
+    public void Clear()
+    {
+        write.text = "";
+    }
+
+}
